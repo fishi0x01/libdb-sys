94fa7a6199eac7b24e95daadf596a6e5bfe5d891
Karl Fischer <fishi0x01@gmail.com>
2020-02-23 15:04:48 +0100

    Add 5.3 and enable feature builds

---
+++ build_5_3.rs
@@ -0,0 +1,40 @@
+extern crate bindgen;
+
+use std::path::{PathBuf, Path};
+use std::process::Command;
+
+pub const SOURCE_DIR: &'static str = "db-5.3.21";
+
+pub fn build_unix(out_dir: &str) {
+    let build_dir = Path::new(SOURCE_DIR).join("build_unix");
+    Command::new("../dist/configure")
+        .arg(&format!("--prefix={}", out_dir))
+        .arg("--with-pic")
+        .current_dir(&build_dir)
+        .status()
+        .unwrap();
+    Command::new("make")
+        .current_dir(&build_dir)
+        .status()
+        .unwrap();
+    Command::new("make")
+        .arg(&"install")
+        .current_dir(&build_dir)
+        .status()
+        .unwrap();
+
+    println!("cargo:rustc-link-search=native={}", Path::new(&out_dir).join("lib").to_string_lossy());
+    println!("cargo:rustc-link-lib=static=db");
+}
+
+pub fn generate_bindings(out_dir: &str) {
+    let bindings = bindgen::Builder::default()
+        .header("db-5.3.21/build_unix/db.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(&out_dir);
+    bindings
+        .write_to_file(out_path.join("ffi_5_3.rs"))
+        .expect("Couldn't write bindings!");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
-//! FFI Bindings for Berkeley DB 4.8.
-
+//! FFI Bindings for Berkeley DB.
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
 extern crate libc;
 
-pub mod ffi;
+#[cfg(feature = "v5_3")]
+include!(concat!(env!("OUT_DIR"), "/ffi_5_3.rs"));
+
+#[cfg(feature = "v4_8")]
+include!(concat!(env!("OUT_DIR"), "/ffi_4_8.rs"));
