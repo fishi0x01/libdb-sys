63891b70f259e4b383849a18ad676df72803e613
Karl Fischer <fishi0x01@gmail.com>
2020-02-25 17:09:59 +0100

    proper default

--- build.rs
+++ build.rs
@@ -6,14 +6,15 @@ mod build_5_3;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    if cfg!(feature = "default") {
-        build_5_3::build_unix(&out_dir);
-        build_5_3::generate_bindings(&out_dir);
-    } else if cfg!(feature = "v5_3") {
+    if cfg!(feature = "v5_3") {
         build_5_3::build_unix(&out_dir);
         build_5_3::generate_bindings(&out_dir);
     } else if cfg!(feature = "v4_8") {
         build_4_8::build_unix(&out_dir);
         build_4_8::generate_bindings(&out_dir);
+    } else {
+        // default
+        build_5_3::build_unix(&out_dir);
+        build_5_3::generate_bindings(&out_dir);
     }
 }
