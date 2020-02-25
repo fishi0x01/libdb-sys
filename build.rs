7715881235507b17e99e522421f4e8c2bebb3f84
Karl Fischer <fishi0x01@gmail.com>
2020-02-25 17:04:17 +0100

    Enable default features

--- build.rs
+++ build.rs
@@ -6,7 +6,10 @@ mod build_5_3;
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    if cfg!(feature = "v5_3") {
+    if cfg!(feature = "default") {
+        build_5_3::build_unix(&out_dir);
+        build_5_3::generate_bindings(&out_dir);
+    } else if cfg!(feature = "v5_3") {
         build_5_3::build_unix(&out_dir);
         build_5_3::generate_bindings(&out_dir);
     } else if cfg!(feature = "v4_8") {
